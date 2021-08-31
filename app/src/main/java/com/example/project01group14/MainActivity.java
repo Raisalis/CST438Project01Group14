f0aa211dbd7de4a625f9d5c0e355997cf4d32ef9
Raisalis <raisalis.lokman@gmail.com>
2021-08-31 10:07:11 -0700

    Basic Login Page rigged.

--- app/src/main/java/com/example/project01group14/MainActivity.java
+++ app/src/main/java/com/example/project01group14/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.project01group14;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Layout pieces
+    private TextView textViewLoginWelcome;
+    private EditText editTextLoginUsername;
+    private EditText editTextLoginPassword;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Attach layout
+        textViewLoginWelcome = findViewById(R.id.textViewLoginWelcome);
+        editTextLoginUsername = findViewById(R.id.editTextLoginUsername);
+        editTextLoginPassword = findViewById(R.id.editTextLoginPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        // When login button is pressed, check entered login information.
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
