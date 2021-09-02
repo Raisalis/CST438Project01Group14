030dcd9c14513a015456b31d8f3f7311fcba54a4
Raisalis <raisalis.lokman@gmail.com>
2021-09-02 10:18:15 -0700

    Calculate Activity rigged.

--- app/src/main/java/com/example/project01group14/MainActivity.java
+++ app/src/main/java/com/example/project01group14/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.project01group14;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Layout pieces
+    private TextView textViewLoginWelcome;
+    private EditText editTextLoginUsername;
+    private EditText editTextLoginPassword;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Attach layout
+        textViewLoginWelcome = findViewById(R.id.textViewLoginWelcome);
+        editTextLoginUsername = findViewById(R.id.editTextLoginUsername);
+        editTextLoginPassword = findViewById(R.id.editTextLoginPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+        // TODO: When login button is pressed, check entered login information.
+        // TODO: If login is correct, send to Main Menu.
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/project01group14/MainMenuActivity.java
@@ -0,0 +1,54 @@
+package com.example.project01group14;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenuActivity extends AppCompatActivity {
+    // Layout pieces
+    private TextView textViewMainMenuWelcome;
+    private Button buttonToCalculatePage;
+    // TODO: Admin page should only be visible if the user is flagged as an admin.
+    private Button buttonToAdminPage;
+    private Button buttonLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainmenu);
+
+        // Attach layout
+        textViewMainMenuWelcome = findViewById(R.id.textViewMainMenuWelcome);
+        buttonToCalculatePage = findViewById(R.id.buttonToCalculatePage);
+        buttonToAdminPage = findViewById(R.id.buttonToAdminPage);
+        buttonLogout = findViewById(R.id.buttonLogout);
+
+        // TODO: Send user to Calculate page
+        buttonToCalculatePage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // TODO: Send user to admin page.
+        buttonToAdminPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // TODO: When logout button is pressed, return to login page.
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
