0a3099640ac3682edb4e450c384ac58707fd910b
Ashley Garcia <32247665+ashgarc20@users.noreply.github.com>
2021-09-09 08:46:29 -0700

    Admin Login and landing page setup

---
+++ app/src/main/java/info/ashley/login/MainActivity.java
@@ -0,0 +1,57 @@
+package info.ashley.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int count = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etName);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView)findViewById(R.id.tvinfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+        Info.setText("Number of attempts remaining: 5");
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+
+
+    }
+    private void validate(String userName, String userPassword) {
+        if ((userName.equals("Admin")) && (userPassword.equals("CST438"))) {
+            Intent intent = new Intent(MainActivity.this, AdminActivity.class);
+            startActivity(intent);
+        } else {
+            count--;
+
+            Info.setText("Number of attempts remaining: " + String.valueOf(count));
+
+
+            if(count == 0) {
+                Login.setEnabled(false);
+            }
+
+        }
+    }
+}
